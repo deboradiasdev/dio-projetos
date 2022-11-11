5d479705110c0c8e2090da8f58908ef58cde7725
Débora <contatodeboradev@gmail.com>
2022-11-11 17:15:46 -0300

    Atividade 2 - Estrutura de repetição

--- loops_e_arrays/src/Ex2_Nota.java
+++ loops_e_arrays/src/Ex2_Nota.java
@@ -6,9 +6,6 @@ import java.util.Scanner;
  * até que o usuário informe um valor válido.
 */
 public class Ex2_Nota {
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
