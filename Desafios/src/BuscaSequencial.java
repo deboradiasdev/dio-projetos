2c6fdfa6cb47e0d9037ce4c5ac37f384e06d28b5
Débora <contatodeboradev@gmail.com>
2022-11-17 14:56:51 -0300

    Entrega dos desafios - Parte 1 - DIO

---
+++ Desafios/src/BuscaSequencial.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+/*Uma busca sequencial se caracteriza por percorrermos todos os 
+elementos de uma estrutura em busca do elemento “X” desejado. Neste desafio, dada uma estrutura de vetor A [a1, a2...an],
+percorra todo o vetor fazendo as comparações de A[N] = X. Caso a condição seja verdadeira, “X” existe na estrutura e a busca será concluída com sucesso.
+Considere um array de 10 elementos do tipo inteiro: 
+Os elementos são {64, 137, -16, 43, 67, 81, -90, 212, 10, 75}. 
+● Carregue esses elementos em um array.  
+● Implementação da busca sequencial: 
+● Crie um método  que realiza uma busca sequencial.  
+● Se o valor constar no array, retorne um texto contendo o valor e sua respectiva posição no array. Caso contrario retorne um texto dizendo que o número não foi encontrado, conforme exemplos abaixo.
+ */
+public class BuscaSequencial {
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+        int posicaoFinal = 0;
+
+        int[] elementos = { 64, 137, -16, 43, 67, 81, -90, 212, 10, 75 };
+        int chave = leitor.nextInt();
+
+        boolean encontrou = false;
+        for (int i = 0; i < elementos.length; i++) {
+            int indice = elementos[i];
+
+            if (indice == chave) {
+                encontrou = true;
+                posicaoFinal = i;
+                break;
+            }
+        }
+        if (encontrou == true) {
+            System.out.println("Achei " + chave + " na posicao " + posicaoFinal);
+
+        } else {
+            System.out.println("Numero " + chave + " nao encontrado!");
+        }
+        leitor.close();
+    }
+}
