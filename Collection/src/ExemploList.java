b6fa671dafdd97ceb076cb9cf823497e5c11f444
Débora <contatodeboradev@gmail.com>
2022-11-18 22:43:51 -0300

    Exercicios de ficação Map!

--- Collection/src/ExemploList.java
+++ Collection/src/ExemploList.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 /*
  * Dada uma lista com 7 notas de um aluno [7, 8.5, 9.3, 5, 7, 0, 3.6], faça:
--- Collection/src/ExemploOrdenacaoList.java
+++ Collection/src/ExemploOrdenacaoList.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 /*Dadas as seguintes informações sobre meus gatos, crie uma lista
  * e ordene esta lista exibindo:
